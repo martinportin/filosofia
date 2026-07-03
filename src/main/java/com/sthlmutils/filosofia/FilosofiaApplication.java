c1141f874e739224283c4d018f0351e59b2bc783
martinportin <martinportin@gmail.com>
2026-07-03 22:19:59 +0200

    1st commit: Initial project

---
+++ src/main/java/com/sthlmutils/filosofia/FilosofiaApplication.java
@@ -0,0 +1,13 @@
+package com.sthlmutils.filosofia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilosofiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilosofiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sthlmutils/filosofia/FilosofiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthlmutils.filosofia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilosofiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
